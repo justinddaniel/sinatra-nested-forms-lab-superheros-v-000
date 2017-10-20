7422e4ebc2a94280c9927b5d3fb8a4e453ee1566
Justin Daniel <justinddaniel@gmail.com>
2017-10-20 15:18:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,13 @@ class App < Sinatra::Base
     end
 
     post '/team' do
+      @team = Team.new(params[:team])
+
+      params[:team][:heroes].each do |args|
+        Hero.new(args)
+      end
+
+      @heroes = Hero.all
       erb :team
     end
 
